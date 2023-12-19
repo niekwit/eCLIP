b944be7bbfe4c4932c8e3525ae597d4faa0db4b3
niekwit <41302859+niekwit@users.noreply.github.com>
2023-12-19 15:37:19 +0000

    updated

--- workflow/scripts/annotate_regions.sh
+++ workflow/scripts/annotate_regions.sh
@@ -4,14 +4,11 @@
 BED=${snakemake_input[bed]}
 BED_HOMER=${snakemake_output[bed]}
 
-echo $BED
-echo $BED_HOMER
-
 awk 'BEGIN{OFS=FS="\t"} {print "chr"$1,$2,$3,"region_"NR,".",$6}' $BED > $BED_HOMER 2> ${snakemake_log[0]}
 
 # annotate regions with HOMER and perform GO analysis
 ANNOTATED=${snakemake_output[txt]}
 
+#annotatePeaks.pl $BED_HOMER ${snakemake_params[genome]} > $ANNOTATED
 annotatePeaks.pl $BED_HOMER ${snakemake_params[genome]} -go ${snakemake_output[go]} > $ANNOTATED 2>> ${snakemake_log[0]}
-
 
